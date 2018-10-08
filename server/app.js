6a3f69b959a978e49ece510487cd79c5c212c8e3
Viral Solani <viral.solani@gmail.com>
2018-10-08 18:19:25 +0530

    Remove unwanted code from app.js

--- server/app.js
+++ server/app.js
@@ -25,21 +25,4 @@ app.use(bodyParser.urlencoded({
 
 app.use('/', routes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-// no stacktraces leaked to user unless in development environment
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: (app.get('env') === 'development') ? err : {}
-  });
-});
-
 module.exports = app;
