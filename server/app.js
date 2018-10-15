b925dd28fcc9802dff4663861c57d7108c535c69
Viral Solani <viral.solani@gmail.com>
2018-10-15 16:07:06 +0530

    - Configure app as suggested in express 4 document. - install require package

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
+const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-const routes = require('./routes/index');
+const indexRouter = require('./routes/index');
 
 /*  const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 if (env === 'development' || env === 'test') {
@@ -13,8 +16,17 @@ if (env === 'development' || env === 'test') {
 // Set up the express app
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 // Log requests to the console.
 app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
@@ -22,6 +34,23 @@ app.use(bodyParser.urlencoded({
   extended: false,
 }));
 
-app.use('/', routes);
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
+
+
+/*
 router.get('/', (req, res) => res.status(200).send({
   message: 'Welcome to the first page.',
 }));
@@ -9,5 +16,5 @@ router.get('/', (req, res) => res.status(200).send({
 router.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
-
+*/
 module.exports = router;
