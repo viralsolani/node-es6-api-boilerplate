1f7fcd60ea0a8c2ede5dde369a11cdb58ce493c3
Viral Solani <viral.solani@gmail.com>
2018-10-18 13:22:36 +0530

    configure babel7

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
 
 const indexRouter = require('./routes/index');
 
